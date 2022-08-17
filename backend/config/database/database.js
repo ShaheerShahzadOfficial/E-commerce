19bdaf8be5b26e12b82b9c0a88f43dc8d0e066e4
Shaheer Shahzad Official <mohammedshaheershahzadrawda5@gmail.com>
2022-08-17 14:07:53 +0500

    Update database.js

--- backend/config/database/database.js
+++ backend/config/database/database.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const DBConnection = () => {
-    // const url = process.env.DB_URI
+     const url = process.env.DB_URI
 
-    mongoose.connect("mongodb+srv://ShaheerShahzad:ShaheerDev@ecommerce.w8dyp.mongodb.net/ForeverFashion?retryWrites=true&w=majority", { useNewUrlParser: true }).then((result) => {
+    mongoose.connect(url, { useNewUrlParser: true }).then((result) => {
         console.log(`DATABASE CONNECTED WITH THE HOST ${result.connection.host}`)
     })
 
